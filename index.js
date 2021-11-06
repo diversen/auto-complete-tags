c03cd8fcecee2ab64f8cd7a6ba704a96b2da1649
Dennis Iversen <dennis.iversen@gmail.com>
2021-11-06 12:47:05 +0100

    auto commit

--- index.js
+++ index.js
@@ -1,203 +1,202 @@
 class AutoCompleteTags extends HTMLElement {
 
-	attributeChangedCallback() {}
+    attributeChangedCallback() { }
 
-	/**
-	 * Called when element is created
-	 */
-	constructor() {
-		super()
-	}
+    /**
+     * Called when element is created
+     */
+    constructor() {
+        super()
+    }
 
-	/**
-	 * Called after the element is attached to the DOM
-	 */
-	connectedCallback() {
+    /**
+     * Called after the element is attached to the DOM
+     */
+    connectedCallback() {
 
-		console.log('connected')
-		const name = this.getAttribute("name")
-		this.innerHTML = `
+        console.log('connected')
+        const name = this.getAttribute("name")
+        this.innerHTML = `
 			<span class="entered"></span>
 			<input name="${name}" type="text" class="enter" value="">
 			<div class="suggest"></div>
 		`
 
-		this.enter = this.querySelector('.enter');
-		this.suggest = this.querySelector('.suggest')
-		this.entered = this.querySelector('.entered');
-		this.loadedTags = []
+        this.enter = this.querySelector('.enter');
+        this.suggest = this.querySelector('.suggest')
+        this.entered = this.querySelector('.entered');
+        this.loadedTags = []
 
-		// read from auto-complete data-url
-		const suggestUrl = this.dataset.url;
+        // read from auto-complete data-url
+        const suggestUrl = this.dataset.url;
 
-		// function for fetching suggestions
-		async function loadTags(search) {
-			const response = await fetch(suggestUrl + search);
-			const names = await response.json();
-			return names
-		}
+        // function for fetching suggestions
+        async function loadTags(search) {
+            const response = await fetch(suggestUrl + search);
+            const names = await response.json();
+            return names
+        }
 
-		// Default wait time before autocomplete tries to load tags from URL
-		let waitTime = 500;
-		if (this.dataset.wait){
-			waitTime = this.dataset.wait;
-		}
+        // Default wait time before autocomplete tries to load tags from URL
+        let waitTime = 500;
+        if (this.dataset.wait) {
+            waitTime = this.dataset.wait;
+        }
 
-		let completeKeys = ['Enter']
-		if (this.dataset.completeKeys) {
-			completeKeys = this.dataset.completeKeys.split(',');
-		}
+        let completeKeys = ['Enter']
+        if (this.dataset.completeKeys) {
+            completeKeys = this.dataset.completeKeys.split(',');
+        }
 
-		// Read from auto-complete value
-		this.enter.value = this.getAttribute('value');
-		this.skipLoadTags = false;
+        // Read from auto-complete value
+        this.enter.value = this.getAttribute('value');
+        this.skipLoadTags = false;
 
-		this.enter.addEventListener('keydown', async (e) => {
+        this.enter.addEventListener('keydown', async (e) => {
 
-			// Get selected from drop down list
-			if (completeKeys.includes(e.key)) {
-				this.setSelectedTag()
-				this.skipLoadTags = true;
-				e.preventDefault();
-			}
+            // Get selected from drop down list
+            if (completeKeys.includes(e.key)) {
+                this.setSelectedTag()
+                this.skipLoadTags = true;
+                e.preventDefault();
+            }
 
-			// Prevent moving to start of input field
-			if (e.key == 'ArrowUp') {
-				e.preventDefault();
-			}
-		})
+            // Prevent moving to start of input field
+            if (e.key == 'ArrowUp') {
+                e.preventDefault();
+            }
+        })
 
-		// The current position in the input list
-		let position = -1;
-		let timer = null;
+        // The current position in the input list
+        let position = -1;
+        let timer = null;
 
-		this.enter.addEventListener("keyup", async (e) => {
+        this.enter.addEventListener("keyup", async (e) => {
 
-			if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+            if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
 
-				e.preventDefault()
+                e.preventDefault()
 
-				const suggestions = document.querySelectorAll('.suggest > div');
-				const num_elems = suggestions.length;
+                const suggestions = document.querySelectorAll('.suggest > div');
+                const num_elems = suggestions.length;
 
-				for (let i = 0; i < num_elems; i++) {
-					suggestions[i].classList.remove('selected');
-				}
+                for (let i = 0; i < num_elems; i++) {
+                    suggestions[i].classList.remove('selected');
+                }
 
-				if (e.key === 'ArrowDown' && position < num_elems - 1) {
-					position += 1;
-				}
+                if (e.key === 'ArrowDown' && position < num_elems - 1) {
+                    position += 1;
+                }
 
-				if (e.key === 'ArrowUp' && position > -1) {
-					position -= 1;
-				}
+                if (e.key === 'ArrowUp' && position > -1) {
+                    position -= 1;
+                }
 
-				if (position >= 0) {
-					let current = suggestions[position]
-					current.classList.add('selected')
-				}
-			} else {
-	
-				// Build suggestion drop down
-				position = -1;
-				let current = this.getCurrentInput()
+                if (position >= 0) {
+                    let current = suggestions[position]
+                    current.classList.add('selected')
+                }
+            } else {
 
-				if (this.skipLoadTags) {
-					current = ''
-				}
+                // Build suggestion drop down
+                position = -1;
+                let current = this.getCurrentInput()
 
-				clearTimeout(timer);
-                timer = setTimeout( async () => {
-					this.loadedTags = await loadTags(current)
-					this.buildTagsList()
-					this.skipLoadTags = false;
-				}, waitTime);
+                if (this.skipLoadTags) {
+                    current = ''
+                }
 
-			}
-		}, false);
-	}
+                clearTimeout(timer);
+                timer = setTimeout(async () => {
+                    this.loadedTags = await loadTags(current)
+                    this.buildTagsList()
+                    this.skipLoadTags = false;
+                }, waitTime);
+            }
+        }, false);
+    }
 
-	/**
-	 * Get selected tag from dropdown
-	 */
-	setSelectedTag() {
-		let selectedTag = this.getSelectedTag();
-		// If not selected then the value is manually input
-		
-		if (!selectedTag) {
-			selectedTag = this.getCurrentInput();
-		}
+    /**
+     * Get selected tag from dropdown
+     */
+    setSelectedTag() {
+        let selectedTag = this.getSelectedTag();
+        // If not selected then the value is manually input
 
-		let allTags = this.getAllTags();
-		allTags.pop();
-		allTags.push(selectedTag)
+        if (!selectedTag) {
+            selectedTag = this.getCurrentInput();
+        }
 
-		this.setTagsString(allTags)
-		
-	}
+        let allTags = this.getAllTags();
+        allTags.pop();
+        allTags.push(selectedTag)
 
-	setTagsString(allTags) {
+        this.setTagsString(allTags)
 
-		allTags = allTags.map( val => val.trim())
-		allTags = Array.from(new Set(allTags));
-		
-		this.enter.value = allTags.join(', ')
-		if (this.enter.value !== '') {
-			this.enter.value += ', '
-		}
-	}
+    }
 
-	deleteTag() {
+    setTagsString(allTags) {
 
-		let allTags = this.getAllTags();
-		allTags.pop();
-		this.setTagsString(allTags)
-	}
+        allTags = allTags.map(val => val.trim())
+        allTags = Array.from(new Set(allTags));
 
-	getSelectedTag() {
-		const elem_selected = this.querySelector('.selected');
-		if (elem_selected) {
-			return elem_selected.innerHTML;
-		}
-	}
+        this.enter.value = allTags.join(', ')
+        if (this.enter.value !== '') {
+            this.enter.value += ', '
+        }
+    }
 
-	getAllTags() {
-		let value = this.enter.value
-		let allTags = value.split(',')
-		
-		allTags = allTags.filter( (val) => {
-			if (val.trim() !== '') {
-				return true
-			}
-		})
+    deleteTag() {
 
-		allTags = allTags.map( val => val.trim())
-		return allTags;
-	}
+        let allTags = this.getAllTags();
+        allTags.pop();
+        this.setTagsString(allTags)
+    }
+
+    getSelectedTag() {
+        const elem_selected = this.querySelector('.selected');
+        if (elem_selected) {
+            return elem_selected.innerHTML;
+        }
+    }
+
+    getAllTags() {
+        let value = this.enter.value
+        let allTags = value.split(',')
+
+        allTags = allTags.filter((val) => {
+            if (val.trim() !== '') {
+                return true
+            }
+        })
+
+        allTags = allTags.map(val => val.trim())
+        return allTags;
+    }
 
 
-	getCurrentInput() {
-		
-		let enteredTags = this.getAllTags();
-		let current = enteredTags.pop();
-		if (!current) {
-			current = '';
-		}
-		return current.trim();
-	}
+    getCurrentInput() {
 
-	buildTagsList() {
+        let enteredTags = this.getAllTags();
+        let current = enteredTags.pop();
+        if (!current) {
+            current = '';
+        }
+        return current.trim();
+    }
 
-		this.suggest.innerHTML = ``;
-		this.loadedTags.forEach((title) => {
-			const li = document.createElement("div");
-			li.appendChild(document.createTextNode(title));
-			li.addEventListener('click', function (e) {
-				// Maybe allow click add at some time
-			})
-			this.suggest.appendChild(li);
-		})
-	}
+    buildTagsList() {
+
+        this.suggest.innerHTML = ``;
+        this.loadedTags.forEach((title) => {
+            const li = document.createElement("div");
+            li.appendChild(document.createTextNode(title));
+            li.addEventListener('click', function (e) {
+                // Maybe allow click add at some time
+            })
+            this.suggest.appendChild(li);
+        })
+    }
 }
 
 export { AutoCompleteTags }
